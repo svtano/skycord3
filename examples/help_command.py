195281249183c6364ab036fad0f2e735b02db5ed
svtano <141485915+svtano@users.noreply.github.com>
2023-11-21 09:34:27 +0100

    Update help_command.py

--- examples/help_command.py
+++ examples/help_command.py
@@ -1,6 +1,6 @@
-import ezcord
+import skycord
 
-bot = ezcord.Bot()
+bot = skycord.Bot()
 bot.add_help_command()
 
 if __name__ == "__main__":
@@ -9,16 +9,16 @@ if __name__ == "__main__":
 
 
 # You can pass values for the help command in cogs like this:
-class Example(ezcord.Cog, name="Example", description="This is a description", emoji="🐍"):
+class Example(skycprd.Cog, name="Example", description="This is a description", emoji="🐍"):
     ...
 
 
 # You can disable the help command for a cog like this:
-class Hidden(ezcord.Cog, hidden=True):
+class Hidden(skycord.Cog, hidden=True):
     ...
 
 
 # You can also group all commands in a cog with another existing cog:
 # This will use the name and emoji of the grouped cog
-class Grouped(ezcord.Cog, group="Example"):
+class Grouped(skycord.Cog, group="Example"):
     ...
