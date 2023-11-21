87b0de2d165fb7418d9ce8b9d29ef593945edeed
svtano <141485915+svtano@users.noreply.github.com>
2023-11-21 09:24:24 +0100

    Update errors.py

--- ezcord/errors.py
+++ ezcord/errors.py
@@ -1,19 +1,19 @@
-class EzcordException(Exception):
-    """Base exception class for all Ezcord exceptions."""
+class SkycordException(Exception):
+    """Base exception class for all Skycord exceptions."""
 
 
-class MissingDiscordLibrary(EzcordException):
+class MissingDiscordLibrary(SkycordException):
     """Raised when no discord library is found."""
 
     def __init__(self):
         super().__init__("No discord library found. Please install a supported library.")
 
 
-class ConvertTimeError(EzcordException):
+class ConvertTimeError(SkycordException):
     """Raised when a time conversion fails."""
 
 
-class Blacklisted(EzcordException):
+class Blacklisted(SkycordException):
     """Can be raised when a blacklisted user tries to use a command.
 
     This error can be caught in a command error handler to send a custom response.
