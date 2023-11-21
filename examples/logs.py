2d714ef3d5002afaa1d6f88564108429872d48c3
svtano <141485915+svtano@users.noreply.github.com>
2023-11-21 09:33:19 +0100

    Update logs.py

--- examples/logs.py
+++ examples/logs.py
@@ -2,8 +2,8 @@ import logging
 
 from colorama import Fore
 
-import ezcord
-from ezcord import log
+import skycord
+from skycord import log
 
 # overwrite colors for specific log levels
 # this can be done with strings or with colorama
@@ -13,8 +13,8 @@ colors = {
 }
 
 # call this function before creating the bot
-ezcord.set_log(
-    log_format=ezcord.LogFormat.default,
+skycord.set_log(
+    log_format=skycord.LogFormat.default,
     colors=colors,
     webhook_url="WEBHOOK_URL",  # Replace with your webhook URL
 )
@@ -22,11 +22,11 @@ ezcord.set_log(
 log.debug("This is a debug message")
 log.info("This is an info message")
 
-ezcord.custom_log("CUSTOM", "This is a message with a custom log level")
+skycord.custom_log("CUSTOM", "This is a message with a custom log level")
 
-bot = ezcord.Bot()
+bot = skycord.Bot()
 
 if __name__ == "__main__":
     # Load all cogs with a custom log style
-    bot.load_cogs("cogs", log=ezcord.CogLog.default, log_color="green")
+    bot.load_cogs("cogs", log=skycord.CogLog.default, log_color="green")
     bot.run("TOKEN")  # Replace with your bot token
